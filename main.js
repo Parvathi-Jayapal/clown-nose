ef097d2c75ee3744341e3b3504e23123e62d11d3
Parvathi-Jayapal <77677826+Parvathi-Jayapal@users.noreply.github.com>
2021-07-07 12:16:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+  clown_nose=loadImage('https://i.postimg.cc/h4CVckMC/clown-nose.jpg');
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video=createCapture(VIDEO);
+  video.size(300,300);
+   video.hide();
+
+   poseNet=ml5.poseNet(video,modelLoaded);
+   poseNet.on('pose',gotposes);
+}
+function modelLoaded(){
+  posenet.on('pose',gotposes);
+  console.log("Posenet is Initialized");
+}
+
+function draw() {
+ image(video,0,0,300,300);
+  image(clown_nose,noseX,noseY,25,35);
+}
+
+function take_snapshot(){
+  save("myFilter.png");
+}
+function gotposes(results){
+  if(results.length>0)
+  {
+    console.log(results);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    console.log("nose x ="+results[0].pose.nose.x);
+    console.log("nose y ="+results[0].pose.nose.y);
+  }
+}
